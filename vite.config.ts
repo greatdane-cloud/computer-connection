f37db83b72d77eaae2a9b25df69b5c28f355f17c
Jeandre van der Byl <vanderbyljeandre@gmail.com>
2026-04-10 14:41:28 +0200

    Add sitemap.xml via vite-plugin-sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,16 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    sitemap({
+      hostname: 'https://computerconnection.tech',
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
